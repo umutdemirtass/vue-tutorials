c9dff06e43fb6c91ee1651323c5a514ffc4e2125
Umut Demirtaş <umut.demirtas@ankabeta.com>
2021-04-26 11:27:17 +0300

    vue

---
+++ directive-create/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button v-umut:click="tikla">v-umut directive</button>
+  </div>
+</template>
+
+<script>
+export default {
+  directives : {
+    umut : {
+      // eslint-disable-next-line no-unused-vars
+      bind(el,binding,vnode){
+      var type = binding.arg;
+      var func = binding.value;
+      el.addEventListener(type,func);
+       }
+    }
+  },
+  methods: {
+    tikla() {
+      alert("v-umut directive");
+    }
+    
+  }
+}
+</script>
+
+
